59177f2e470b27c20d7ef22f52e1d95d99dbc675
winsupervip <quoclamdn2018@gmail.com>
2023-01-18 20:09:31 +0700

    install prettier,husky,lint-staged

--- src/routes/site.js
+++ src/routes/site.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
-const siteController = require('../app/controllers/SiteController');
-                    router.use('/search', siteController.search);
+const siteController = require("../app/controllers/SiteController");
+router.use("/search", siteController.search);
 
-            router.use('/', siteController.index);
+router.use("/", siteController.index);
 
 module.exports = router;
